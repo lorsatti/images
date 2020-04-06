fdfd710fd5c268e83ded9673bc2ffe8248c7e9f2
Lorenzo Orsatti <49567430+lorsatti@users.noreply.github.com>
2020-04-06 10:35:58 +0200

    remove warnings

--- selenium/android/emulator-snapshot.sh
+++ selenium/android/emulator-snapshot.sh
@@ -11,6 +11,13 @@ adb devices | grep emulator | cut -f1 | while read id; do
             done
         fi
     done
+    java -Xmx1024M -Xss1m -jar /opt/android-sdk-linux/build-tools/29.0.2/lib/apksigner.jar sign \
+        --key /opt/node_modules/appium/node_modules/appium-adb/keys/testkey.pk8 \
+        --cert /opt/node_modules/appium/node_modules/appium-adb/keys/testkey.x509.pem \
+        /opt/node_modules/appium/node_modules/appium-uiautomator2-server/apks/appium-uiautomator2-server-debug-androidTest.apk
+    adb -s "$id" install -g /opt/node_modules/appium/node_modules/io.appium.settings/apks/settings_apk-debug.apk
+    adb -s "$id" install -r /opt/node_modules/appium/node_modules/appium-uiautomator2-server/apks/appium-uiautomator2-server-v*.apk
+    adb -s "$id" install -r /opt/node_modules/appium/node_modules/appium-uiautomator2-server/apks/appium-uiautomator2-server-debug-androidTest.apk
     adb -s "$id" emu kill -2 || true
 done
 rm -f /tmp/.X99-lock || true
